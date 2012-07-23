7768326969e02c698e94c1f961bc515995341ead
Elvis_Zhou <zhouxz007@163.com>
2012-07-23 18:57:59 +0800

    mysql插入失败的数据可以在窗体中正常显示

--- insert/mysql.py
+++ insert/mysql.py
@@ -18,14 +18,14 @@ logger = None
 
 def initMysql(_logger, host="localhost",user='root',passwd='123456',db='companyinformation'):
     global  conn, cursor, logger
-
     logger = _logger
     conn=MySQLdb.connect(host,user,passwd,db,charset="utf8")
     cursor=conn.cursor()
 
 
-def inserttoMysql(tuple,tablename='companyinformation'):
+def inserttoMysql(tuple,_logger=logger,tablename='companyinformation',):
     sql="insert into "+tablename+" values(%s,%s,%s,%s,%s,%s)"
+    #logger = _logger
     try:
         n=cursor.execute(sql,tuple)
     except :
@@ -34,8 +34,7 @@ def inserttoMysql(tuple,tablename='companyinformation'):
         logger.info("去重操作：该数据存在重复,主键无法有相同值，已删除原有数据库中该条数据并重新生成:")
         logger.info("ID: %s" % tuple[0])
         logger.info("公司名:%s"%tuple[1])
-
-n=cursor.execute(sql,tuple)
+        n=cursor.execute(sql,tuple)
 
 
 def finishInsertMysql():
@@ -47,6 +46,6 @@ def finishInsertMysql():
 if __name__=='__main__':
     infor=("100000_10720K5DHP9Z","安徽省潜山县信兴刷业有限公司","安徽省潜山县信兴刷业有限公司/销售是一家集生产加工、经销批发的股份有限公司，高品质布轮、工业毛刷.是安徽省潜山县信兴刷业有限公司/销售的主营产品。安徽省潜山县信兴刷业有限公司/销售是一家经国家相关部门批准注册的企业。安徽省潜山县信兴刷业有限公司/销售以雄厚的实力、合理的价格、优良的服务与多家企业建立了长期的合作关系。安徽省潜山县信兴刷业有限公司/销售热诚欢迎各界前来参观、考察、洽谈业务。","高品质布轮;工业毛刷.","http://www.qsxxsy.cn","安徽省潜山县信兴刷业有限公司网站")
     initMysql(logging.getLogger(__name__))
-    inserttoMysql(infor)
+    inserttoMysql(infor,logging.getLogger(__name__))
     finishInsertMysql()
 
